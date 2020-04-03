7978f889a31a9f9193087cb44f7a87453d6b9fe3
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-03 09:13:22 +0000

    アカウント編集機能を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def new
     @user = User.new
   end
@@ -6,22 +8,33 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      redirect_to user_path(@user.id), notice: "ユーザー登録が完了しました。"
+      redirect_to user_path(@user.id), notice: "アカウント登録が完了しました。"
     else
       render :new
     end
   end
 
   def show
-    @user = User.find(params[:id])
   end
 
   def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to user_path, notice: "アカウントを編集しました。"
+    else
+      render :edit
+    end
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image, :comment)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: "blogs#index"
-  
+
   resources :blogs
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 end
