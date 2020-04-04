e9b832651097fd1ace18d9ae6b58b4a0a9444485
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-04 16:34:03 +0000

    サインアップ時にバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :favorite_blogs, through: :favorites, source: :blog
   has_one_attached :profile_image
+
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password, presence: true
+  validates :password_confirmation,  presence: true
 end
