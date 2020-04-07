3cba1c465144b90f16d7ceb6965877016c588542
shohei-hotta <sho1127-cal@kra.biglobe.ne.jp>
2020-04-07 10:41:43 +0000

    メール送信先を投稿者に変更

--- app/mailers/blog_mailer.rb
+++ app/mailers/blog_mailer.rb
@@ -1,6 +1,6 @@
 class BlogMailer < ApplicationMailer
   def blog_mail(blog)
     @blog = blog
-    mail to: "sho1127-cal@kra.biglobe.ne.jp", subject: "投稿の確認メール"
+    mail to: @blog.user.email, subject: "投稿の確認メール"
   end
 end
